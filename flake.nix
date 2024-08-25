aaaa57179e8e0638db8cb4b933be19080edca370
zincentimeter <zincentimeter@outlook.com>
2024-08-25 17:41:39 +0000

    flake, module: init flake files
    • Added input 'nixpkgs':
        'github:NixOS/nixpkgs/c374d94f1536013ca8e92341b540eba4c22f9c62?narHash=sha256-Z/ELQhrSd7bMzTO8r7NZgi9g5emh%2BaRKoCdaAv5fiO0%3D' (2024-08-21)

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Versions: Specify software versions in Nix, elegantly and efficiently";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs }:
+  # https://git.sr.ht/~fubuki/stratosphere/tree/roze/item/flake.nix#L9
+  let
+    systems = [
+      "x86_64-linux"
+      "i686-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      "aarch64-linux"
+      "armv6l-linux"
+      "armv7l-linux"
+    ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in
+  {
+    # https://git.sr.ht/~fubuki/stratosphere/tree/roze/item/flake.nix#L27
+    nixosModules = nixpkgs.lib.mapAttrs (
+      (name: value: import value) (import ./modules)
+    );
+    formatter = forAllSystems (system:
+      nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+    devShells = forAllSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.mkShell {
+          buildInputs = [ pkgs.nix-update pkgs.nix-prefetch-github ];
+        };
+      }
+    );
+  };
+}
